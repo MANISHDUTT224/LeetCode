e1ce70829898f6dc5f4e6b1bfe6a9453fd485abb
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-04 08:26:56 +0530

    Time: 42 ms (99.6%), Space: 56.5 MB (56.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         sort(skill.begin(),skill.end());
         long long ans=0;
         int l=0,r=skill.size()-1;
