1ba8f9fe3089c62c5d6352f9bd3b8ebb9ea73ea2
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-08 19:55:31 +0530

    Time: 262 ms (5.1%), Space: 148.3 MB (5.07%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -8,15 +8,15 @@ public:
         permuterec(nums,0,res);
         return res;
     }
-    vector<vector<int>> permuterec(vector<int>&nums,int ci,vector<vector<int>>&res){
-        if(ci==nums.size()-1){
+    vector<vector<int>> permuterec(vector<int>nums,int ci,vector<vector<int>>&res){
+        if(ci>=nums.size()){
             res.push_back(nums);
             return res;
         }
         for(int i=ci;i<nums.size();i++){
             swap(nums[ci],nums[i]);
             permuterec(nums,ci+1,res);
-            swap(nums[ci],nums[i]);
+            
         }
         return res;
     }
