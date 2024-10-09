2f41c34aecbf4f866995c0351c8a4ee12cb55111
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-09 11:19:14 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int cnt_open=0,cnt=0;
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
                 cnt_open++;
