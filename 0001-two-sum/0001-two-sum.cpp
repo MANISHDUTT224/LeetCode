b5b6fd2b5c357a84d5e617ab1dbca3db01b84efe
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-09 16:41:43 +0530

    Time: 3 ms (98.45%), Space: 14 MB (49.79%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        
+        int l=0,r=arr.size()-1;
+        vector<int>res;
+        vector<pair<int,int>>v;
+        for(int i=0;i<arr.size();i++){
+            v.push_back({arr[i],i});
+        }
+        sort(v.begin(),v.end());
+        while(l<=r){
+            if(v[l].first+v[r].first==target){
+                return {v[l].second,v[r].second};
+            }
+            else if(v[l].first+v[r].first<target){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }   
+        return {-1,-1};
+    }
+};
