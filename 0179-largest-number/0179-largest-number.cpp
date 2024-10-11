d0e8116fe23e04e117ea7455a2de30944739f456
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-11 11:09:27 +0530

    Time: 8 ms (44.25%), Space: 15.5 MB (86.51%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -4,18 +4,16 @@ public:
         return to_string(a)+to_string(b)>to_string(b)+to_string(a);
     }
     string largestNumber(vector<int>& nums) {
-        
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
+        int n=nums.size();
         sort(nums.begin(),nums.end(),comp);
+        string ans="";
         if(nums[0]==0){
             return "0";
         }
-       string ans;
-       for(int n:nums){
-        ans+=to_string(n);
-       }
-       return ans;
+        for(int i:nums){
+            ans+=to_string(i);
+        }
+        return ans;
+        
     }
 };
