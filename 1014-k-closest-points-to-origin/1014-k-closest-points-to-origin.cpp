bc70e47c72f4bfd2686dc0db2193719a5656dc26
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-12 21:00:26 +0530

    Time: 119 ms (93.37%), Space: 68.1 MB (67.18%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    double distance(vector<int> &point){
+    double distance(vector<int>& point){
         return point[0]*point[0]+point[1]*point[1];
     }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-        priority_queue<pair<double,int>,vector<pair<double,int>>>  maxheap;
-        for(int i=0;i<points.size();i++){
-            double newdist=distance(points[i]);
-            if(maxheap.size()<k){
+       vector<vector<int>> res;
+       priority_queue<pair<double,int>,vector<pair<double,int>>>maxheap;
+       for(int i=0;i<points.size();i++){
+        double newdist=distance(points[i]);
+        if(maxheap.size()<k){
+            maxheap.push({newdist,i});
+        }
+        else{
+            if(maxheap.top().first>newdist){
+                maxheap.pop();
                 maxheap.push({newdist,i});
             }
-            else{
-                if(maxheap.top().first>newdist){
-                    maxheap.pop();
-                    maxheap.push({newdist,i});
-                }
-            }
         }
-        vector<vector<int>> res;
-        while(!maxheap.empty()){
-            int index=maxheap.top().second;
-
-            maxheap.pop();
-            res.push_back(points[index]);
-        }
-        return res;
+       }
+       while(!maxheap.empty()){
+        int index=maxheap.top().second;
+        maxheap.pop();
+        res.push_back(points[index]);
+       }
+       return res;
     }
 };
