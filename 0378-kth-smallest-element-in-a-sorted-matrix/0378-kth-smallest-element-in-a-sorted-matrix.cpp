9a448fb3d4069065236ae3c0ffb1576f0c639ee3
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-12 21:09:22 +0530

    Time: 11 ms (99.4%), Space: 18.3 MB (24.93%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         priority_queue<int,vector<int>>maxheap;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[i].size();j++){
