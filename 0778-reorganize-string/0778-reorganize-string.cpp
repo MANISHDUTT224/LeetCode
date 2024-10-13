25098d5920d8812d133cf00d73f0e7e075f8c75f
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-13 08:21:40 +0530

    Time: 0 ms (100%), Space: 8.3 MB (63.52%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        map<char,int>mp;
-
         int count[26]={0};
         for(char c:s){
             count[c-'a']++;
         }
-        int n=s.size();
         auto compare=[](pair<int,char>&x,pair<int,char>&y){
             if(x.first==y.first){
                 return x.second>y.second;
@@ -16,37 +13,35 @@ public:
                 return x.first<y.first;
             }
         };
+        int n=s.size();
         priority_queue<pair<int,char>,vector<pair<int,char>>,decltype(compare)>pq(compare);
         for(int i=0;i<26;i++){
             if(count[i]>0){
-
-            if(count[i]>(n+1)/2){
-                return "";
+                if(count[i]>(n+1)/2){
+                    return "";
+                }
+                pq.push({count[i],(char)(i+'a')});
             }
-            pq.push({count[i],(char)(i+'a')});
-            }
-
         }
         string res="";
-       while(pq.size()>=2){
-        pair<int,char>pair1=pq.top();
-        pq.pop();
-        pair<int,char>pair2=pq.top();
-        pq.pop();
-        res+=pair1.second;
-        res+=pair2.second;
-        if(--pair1.first>0){
-            pq.push(pair1);
+        while(pq.size()>=2){
+            pair<int,char> pair1=pq.top();
+            pq.pop();
+            pair<int,char>pair2=pq.top();
+            pq.pop();
+            res+=pair1.second;
+            res+=pair2.second;
+            if(--pair1.first>0){
+                pq.push(pair1);
+            }
+            if(--pair2.first>0){
+                pq.push(pair2);
+            }
         }
-        if(--pair2.first>0){
-            pq.push(pair2);
+        if(pq.size()>0){
+            res+=pq.top().second;
+            pq.pop();
         }
-
-       }
-       if(pq.size()>0){
-        res+=pq.top().second;
-        pq.pop();
-       }
-       return res;
+        return res;
     }
 };
