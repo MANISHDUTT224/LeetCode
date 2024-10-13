d9578ccd4167d58c01cc783d09aa9b7ba38e1a0e
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-13 07:40:11 +0530

    Time: 11 ms (51.19%), Space: 11.2 MB (55.78%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-        map<char,int>mp;
-        for(char c:s){
-            mp[c]++;
+       map<char,int> mp;
+       for(char c:s){
+        mp[c]++;
+       } 
+       priority_queue<pair<int,char>,vector<pair<int,char>>>minheap;
+       for(auto it:mp){
+        minheap.push({it.second,it.first});
+       }
+       string res="";
+       while(!minheap.empty()){
+        int charfreq=minheap.top().first;
+        char ch=minheap.top().second;;
+        for(int i=0;i<charfreq;i++){
+            res+=ch;
         }
-        priority_queue<pair<int,char>,vector<pair<int,char>>>maxheap;
-        for(auto it:mp){
-            maxheap.push({it.second,it.first});
-        }
-        string res="";
-        while(!maxheap.empty()){
-            int chfreq=maxheap.top().first;
-            char ch=maxheap.top().second;
-            for(int i=0;i<chfreq;i++){
-
-                res+=ch;
-            }
-            maxheap.pop();
-        }
-        return res;
+        minheap.pop();
+       }
+       return res;
     }
 };
