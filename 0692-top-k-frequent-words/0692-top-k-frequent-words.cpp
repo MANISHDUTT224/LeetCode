0d9324ac5038a88f07b27fa4391457c118101191
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-13 08:13:20 +0530

    Time: 12 ms (48.33%), Space: 17.6 MB (31.93%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -9,23 +9,27 @@ public:
                 return x.second>y.second;
             }
         };
-        unordered_map<string,int> mp;
-        priority_queue<pair<string,int>,vector<pair<string,int>>,decltype(compare)>maxheap(compare);
+        priority_queue<pair<string,int>,vector<pair<string,int>>,decltype(compare)>minheap(compare);
+        unordered_map<string,int>mp;
         for(string s:words){
             mp[s]++;
         }
         for(auto it:mp){
-            maxheap.push({it.first,it.second});
-            if(maxheap.size()>k){
-                maxheap.pop();
+            minheap.push({it.first,it.second});
+            if(minheap.size()>k){
+                
+                    minheap.pop();
+                   
+                
             }
         }
-        vector<string>res(k);
-        int index=k-1;
-        while(!maxheap.empty()){
-            res[index--]=maxheap.top().first;
-            maxheap.pop();
+        vector<string>res;
+      
+        while(!minheap.empty()){
+            res.push_back(minheap.top().first);
+            minheap.pop();
         }
+        reverse(res.begin(),res.end());
         return res;
     }
 };
