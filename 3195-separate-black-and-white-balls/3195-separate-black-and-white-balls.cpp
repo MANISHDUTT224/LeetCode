4f57a0ce18a73ea761c6030ef9e81590a255f70c
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-10-15 10:04:24 +0530

    Time: 16 ms (98.53%), Space: 15.8 MB (14.11%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
+        cin.tie(0);
+        cout.tie(0);
+
+        ios::sync_with_stdio(false);
     long long ans=0,c=0;
     for(int i=0;i<s.size();i++){
         if(s[i]=='1'){
