8e5a01244c34c6e9a61ec064c738035fc67519e4
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-11-07 12:18:38 +0530

    Time: 86 ms (95.15%), Space: 84.7 MB (90.57%) - LeetHub

--- 2310-minimum-operations-to-halve-array-sum/2310-minimum-operations-to-halve-array-sum.cpp
+++ 2310-minimum-operations-to-halve-array-sum/2310-minimum-operations-to-halve-array-sum.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int halveArray(vector<int>& nums) {
         double sum=0,s;
-        priority_queue<double> pq;
+        priority_queue<double> pq(nums.begin(),nums.end());
 
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            pq.push(nums[i]);
+            
         }
         s=sum;
         int ans=0;
-        while(s>=sum/2.0){
+        while(s>(sum/2.0)){
             double maxnum=pq.top();
             pq.pop();
             s-=(maxnum/2.0);
