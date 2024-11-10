426ca1af22dded27e1c7e72b1343dc813ed40ad6
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-11-10 09:17:52 +0530

    Time: 53 ms (72.53%), Space: 88.3 MB (74.37%) - LeetHub

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>bitcount(32,0);
+        int left=0,currentor=0;
+        int minlength=INT_MAX;
+        for(int right=0;right<n;right++){
+            currentor|=nums[right];
+            for(int i=0;i<32;i++){
+                if(nums[right] & (1<<i)){
+                    bitcount[i]++;
+                }
+            }
+            while(left<=right && currentor>=k){
+                minlength=min(minlength,right-left+1);
+                int updatedor=0;
+                for(int i=0;i<32;i++){
+                    if(nums[left] & (1<<i)){
+                        bitcount[i]--;
+                    }
+                    if(bitcount[i]>0){
+                        updatedor|=(1<<i);
+                    }
+                }
+                currentor=updatedor;
+                left++;
+            }
+        }
+        return minlength==INT_MAX?-1:minlength;
+
+    }
+};
