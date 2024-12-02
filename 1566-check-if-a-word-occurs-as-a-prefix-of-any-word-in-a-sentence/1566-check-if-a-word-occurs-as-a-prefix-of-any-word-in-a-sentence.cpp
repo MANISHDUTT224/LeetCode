66c76a65ab4aee7f6f259933985dde17e00071bd
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-12-02 16:28:52 +0530

    Time: 0 ms (100%), Space: 8.2 MB (6.42%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
         string s=" "+sentence,t=" "+searchWord;
-        int n=s.size();
         int m=s.find(t);
         if(m==-1){
             return -1;
