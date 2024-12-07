5ce2626fe2daef6907f97531b99dbccc9a32d02c
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-12-07 22:01:14 +0530

    Time: 23 ms (96.67%), Space: 59.7 MB (22.28%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minimumSize(vector<int>& nums, int maxOperations) {
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         int low=1,high=*max_element(nums.begin(),nums.end());
         while(low<high){
             int mid=low+(high-low)/2;
