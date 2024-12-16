b6a99b13c2fc7812f80e4fa33972afb6aaacfc3a
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2024-12-16 19:42:26 +0530

    Time: 4 ms (39.26%), Space: 30.5 MB (23.64%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
+        
         set<int>points;
         for(int i=0;i<nums.size();i++){
             for(int j=nums[i][0];j<=nums[i][1];j++){
