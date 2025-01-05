b7c8b53c9648a5de55435b18ccac322a06dcfa82
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-05 17:12:04 +0530

    Time: 10 ms (23.23%), Space: 117.9 MB (73.23%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-        const int m=rolls.size();
-        const int srolls=accumulate(rolls.begin(),rolls.end(),0);
-        const int total=(n+m)*mean,miss=total-srolls;
-        if(miss>6*n || miss<n){
+        int knownrollsum=0;
+        for(int roll:rolls){
+            knownrollsum+=roll;
+        }
+        int totalrollsum=mean*(rolls.size()+n);
+        int unknownrollsum=totalrollsum-knownrollsum;
+        int val=unknownrollsum/n;
+        int rem=unknownrollsum%n;
+        if(unknownrollsum<n || unknownrollsum>n*6){
             return {};
         }
-        auto [q,r]=div(miss,n);
-        vector<int> ans(n,q);
-        fill(ans.begin(),ans.begin()+r,q+1);
-        return ans;
+        vector<int>result(n,0);
+        for(int i=0;i<n;i++){
+            result[i]=val+(i<rem?1:0);
+        }
+        return result;
     }
 };
