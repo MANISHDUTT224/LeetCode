fae054e82debe53ad741ad635b18b2b8808ac5e4
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-05 21:53:01 +0530

    Time: 8 ms (34.6%), Space: 118.1 MB (42.17%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -7,14 +7,14 @@ public:
         }
         int totalrollsum=mean*(rolls.size()+n);
         int unknownrollsum=totalrollsum-knownrollsum;
-        int val=unknownrollsum/n;
-        int rem=unknownrollsum%n;
-        if(unknownrollsum<n || unknownrollsum>n*6){
+        if(unknownrollsum<n || unknownrollsum>6*n){
             return {};
         }
+        int value=unknownrollsum/n;
+        int extra=unknownrollsum%n;
         vector<int>result(n,0);
         for(int i=0;i<n;i++){
-            result[i]=val+(i<rem?1:0);
+            result[i]=value+(i<extra?1:0);
         }
         return result;
     }
