8781d46ff00fb4040b42329c47e09dd2cfdaf7c3
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-06 10:22:44 +0530

    Time: 4 ms (31.13%), Space: 23.6 MB (12.7%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i=0;
+        string res;
+        int n=s.size();
+        while(i<n){
+            while(i<n && s[i]==' '){
+                i++;
+            }
+            if(i==n){
+                break;
+            }
+            int j=i+1;
+            while(j<n && s[j]!=' '){
+                j++;
+            }
+            string w=s.substr(i,j-i);
+            if(res.size()==0){
+                res=w;
+            }
+            else{
+                res=w+" "+res;
+            }
+            i=j+1;
+        }
+        return res;
+    }
+};
