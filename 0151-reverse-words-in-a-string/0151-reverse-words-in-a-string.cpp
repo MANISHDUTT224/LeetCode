0506ff05940c6131a7f4ac5140ecd36e9d0ba88d
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-06 14:39:22 +0530

    Time: 38 ms (6.5%), Space: 23.4 MB (18.71%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-        int i=0;
-        string res;
-        int n=s.size();
+        int n=s.size(),i=0;
+        string result="";
+       
         while(i<n){
+           
             while(i<n && s[i]==' '){
                 i++;
             }
@@ -18,15 +16,17 @@ public:
             while(j<n && s[j]!=' '){
                 j++;
             }
-            string w=s.substr(i,j-i);
-            if(res.size()==0){
-                res=w;
+            string sub=s.substr(i,j-i);
+            cout<<sub<<i<<j<<endl;
+            if(result.size()==0){
+                result=sub;
             }
             else{
-                res=w+" "+res;
+                result=sub+" "+result;
             }
+            
             i=j+1;
         }
-        return res;
+        return result;
     }
 };
