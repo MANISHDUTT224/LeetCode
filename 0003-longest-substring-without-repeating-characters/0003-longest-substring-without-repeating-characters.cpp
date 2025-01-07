e16d6146ba2ce72c359f9668f6d3d39ab916a876
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-07 21:44:09 +0530

    Time: 23 ms (22.12%), Space: 16.9 MB (10.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-        int l=0,r=0,n=s.size();
+        int l=0,r=0;
+        int n=s.size();
         set<char>st;
-        int maxl=0;
+        int curl,maxl=0;
         while(r<n){
             if(st.find(s[r])==st.end()){
                 st.insert(s[r]);
-                int curl=r-l+1;
-                maxl=max(maxl,curl);
+                curl=r-l;
+                maxl=max(maxl,curl+1);
                 r++;
             }
             else{
                 st.erase(s[l]);
                 l++;
             }
+        
         }
         return maxl;
     }
