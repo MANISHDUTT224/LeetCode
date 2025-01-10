99c2b5ada01ad95f04f5d1bbecdad68cb878edc9
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-10 16:19:12 +0530

    Time: 321 ms (14.16%), Space: 168.6 MB (7.76%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        map<char,int>mp;
+        for(auto it:words2){
+            map<char,int>temp;
+            for(auto i:it){
+                temp[i]++;
+            }
+            for(auto it1:temp){
+                char chr=it1.first;
+                int freq=it1.second;
+                mp[chr]=max(mp[chr],temp[chr]);
+            }
+
+        }
+        vector<string>ans;
+        for(auto it:words1){
+            map<char,int>temp2;
+            for(int i:it){
+                temp2[i]++;
+            }
+            bool flag=1;
+            for(auto it1:mp){
+                char c=it1.first;
+                int freq1=it1.second;
+                if(temp2[c]<freq1){
+                    flag=0;
+                    break;
+                }
+            }
+            if(flag){
+                ans.push_back(it);
+            }
+
+        }
+        return ans;
+    }
+
+};
