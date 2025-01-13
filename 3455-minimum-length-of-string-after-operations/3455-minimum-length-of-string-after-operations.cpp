39758180e4c93249a70ec88207c7d588ee3b89c8
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-01-13 11:11:22 +0530

    Time: 19 ms (62.5%), Space: 32.1 MB (74.19%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minimumLength(string s) {
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         int totallength=0;
         vector<int>charfreq(26,0);
         for(char c:s){
