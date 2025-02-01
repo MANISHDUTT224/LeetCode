f5f4ffb9c003a5033f5f9506258986d1ce27309c
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-02-01 11:13:08 +0530

    Time: 0 ms (100%), Space: 17.7 MB (98.29%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        for(int i:nums){
-            mp[i]++;
+      unordered_map<int,int>mp;
+      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>maxheap;
+      for(auto num:nums){
+        mp[num]++;
+      }
+      vector<int>res;
+      for(auto it:mp){
+        if(maxheap.size()<k){
+            maxheap.push({it.second,it.first});
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>maxheap;
-        for(auto it:mp){
-            if(maxheap.size()<k){
-                maxheap.push({it.second,it.first});
-            }
-            else{
-                if(maxheap.top().first<it.second){
-                    maxheap.pop();
-                    maxheap.push({it.second,it.first});
-                }
-            }
-        }
-        vector<int>res;
-        while(!maxheap.empty()){
-            res.push_back(maxheap.top().second);
+        else  if(maxheap.top().first<it.second){
             maxheap.pop();
-        }return res;
+            maxheap.push({it.second,it.first});
+        }
+      }
+      while(!maxheap.empty()){
+        res.push_back(maxheap.top().second);
+        maxheap.pop();
+
+      }
+      return res;
     }
 };
