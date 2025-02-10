93cd287069028e0d94361539a3a120f5bdad566d
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-02-11 01:19:05 +0530

    Time: 39 ms (58.07%), Space: 74.8 MB (28.6%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int end=0,start=0;
+    string longestPalindrome(string s) {
+        int n=s.size();
+        helper(s,0,n);
+        return s.substr(start,end-start+1);
+    }
+    void helper(string s,int i,int n){
+        if(i>=n){
+            return;
+        }
+        int left=i,right=i;
+        while(right<n-1 && s[right]==s[right+1]){
+            right++;
+        }
+        i=right;
+        while(left>0 && right<n-1 && s[left-1]==s[right+1]){
+            left--;
+            right++;
+        }
+        if(end-start<right-left){
+            start=left;
+            end=right;
+        }
+        helper(s,++i,n);
+
+    }
+};
