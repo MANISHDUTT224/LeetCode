5eca91adfc278e14f03f975a7c18d23989a6ca85
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-02-13 11:56:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-        vector<int> dp(text1.size(),0);
-        int maxlen=0;
+        vector<int>dp(text1.size(),0);
         for(char c:text2){
             int len=0;
+            int maxlen=0;
             for(int i=0;i<text1.size();i++){
                 if(len<dp[i]){
                     len=dp[i];
                 }
                 else if(c==text1[i]){
                     dp[i]=len+1;
-                    maxlen=max(maxlen,len+1);
+                     maxlen=max(maxlen,len);
                 }
             }
         }
-        return maxlen;
+        return dp[text1.size()-1];
     }
 };
