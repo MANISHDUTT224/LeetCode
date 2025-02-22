67f9233c80cd00a8638910fda2e6692004d4c154
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-02-22 08:57:38 +0530

    Time: 4 ms (50.33%), Space: 21.6 MB (64.33%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        int index=0;
+        return traverse(traversal,index,0);
+    }
+    TreeNode* traverse(const string& traversal,int& index,int depth){
+        if(index>=traversal.size()){
+            return nullptr;
+        }
+        int dashcount=0;
+        while(index+dashcount<traversal.size() && traversal[index+dashcount]=='-'){
+            dashcount++;
+        }
+         if(dashcount!=depth){
+            return nullptr;
+        }
+        index+=dashcount;
+       
+        int value=0;
+        while(index<traversal.size() && isdigit(traversal[index])){
+            value=value*10+(traversal[index]-'0');
+            index++;
+        }
+        TreeNode* node=new TreeNode(value);
+        node->left=traverse(traversal,index,depth+1);
+        node->right=traverse(traversal,index,depth+1);
+        return node;
+    }
+};
