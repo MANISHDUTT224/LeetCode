4a59a03dbadee3abe6b5caf28324e5233d8dd4d7
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-02-24 08:43:26 +0530

    Time: 150 ms (59.96%), Space: 161.2 MB (52.89%) - LeetHub

---
+++ 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+        n=amount.size();
+        tree.resize(n,vector<int>());
+        for(vector<int>edge:edges){
+            tree[edge[0]].push_back(edge[1]);
+            tree[edge[1]].push_back(edge[0]);
+        }
+        distanceFromBob.resize(n);
+        return findpaths(0,0,0,bob,amount);
+
+    }
+private:
+int n;
+vector<vector<int>>tree;
+vector<int>distanceFromBob;
+int findpaths(int sourceNode,int parentNode,int time,int bob,vector<int>&amount){
+    int maxincome=0,maxChild=INT_MIN;
+    if(sourceNode==bob){
+        distanceFromBob[sourceNode]=0;
+    }
+    else{
+        distanceFromBob[sourceNode]=n;
+    }
+    for(int adjacentNode:tree[sourceNode]){
+        if(adjacentNode!=parentNode){
+            maxChild=max(maxChild,findpaths(adjacentNode,sourceNode,time+1,bob,amount));
+            distanceFromBob[sourceNode]=min(distanceFromBob[sourceNode],distanceFromBob[adjacentNode]+1);
+        }
+    }
+    if(distanceFromBob[sourceNode]>time){
+        maxincome+=amount[sourceNode];
+    }
+    else if(distanceFromBob[sourceNode]==time){
+        maxincome+=amount[sourceNode]/2;
+    }
+    if(maxChild==INT_MIN){
+        return maxincome;
+    }
+    return maxincome+maxChild;
+}
+};
