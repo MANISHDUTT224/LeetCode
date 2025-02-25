f7f8a513282a97fc6d95b309574a3a3c76432a48
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-02-25 08:22:39 +0530

    Time: 15 ms (20.27%), Space: 112.3 MB (26.71%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         int count=0,prefixSum=0,oddcount=0,evencount=1;
         const int MOD=1e9+7;
         for(int num:arr){
