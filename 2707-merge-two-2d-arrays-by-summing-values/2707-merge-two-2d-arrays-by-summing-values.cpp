fc63cd09453fc1af6d20f555163ca7ecae0655cc
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-02 09:41:37 +0530

    Time: 4 ms (19.09%), Space: 14.9 MB (38.58%) - LeetHub

---
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        vector<vector<int>>res;
+        set<int>st;
+        for(int i=0;i<nums1.size();i++){
+                    bool found=false;
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i][0]==nums2[j][0]){
+                    nums1[i][1]+=nums2[j][1];
+                   
+                    res.push_back(nums1[i]);
+                    st.insert(nums1[i][0]);
+                    found=true;
+                    break;
+                }
+            }
+            if(!found){
+                res.push_back(nums1[i]);
+
+            }
+        }
+        for(int i=0;i<nums2.size();i++){
+            if(st.find(nums2[i][0])==st.end()){
+                res.push_back(nums2[i]);
+            }
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
