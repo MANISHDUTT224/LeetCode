9806983d264b8ac21502e2a2d3ada91a73592f73
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-16 12:31:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-       if(nums.size()==0){
-        return nums[0];
-       }
-       int n=nums.size();
-       vector<int>dp(n,0);
-       dp[0]=nums[0];
-       int sum=nums[0];
-       for(int i=1;i<n;i++){
-        dp[i]=max(dp[i-1]+nums[i],nums[i]);
-        sum=max(sum,dp[i]);
-       }
-       return sum;
-
-        
+        int sum=0,mxsum=0;
+        if(nums.size()==1){
+            return nums[0];
+        }
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum>mxsum){
+                mxsum=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return mxsum;
     }
 };
