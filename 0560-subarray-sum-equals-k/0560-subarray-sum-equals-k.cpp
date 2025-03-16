d931be35a856e89e52b8ce3b49813e84d0ca34bc
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-16 13:41:48 +0530

    Time: 83 ms (32.56%), Space: 54.1 MB (5.19%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,22 +1,14 @@
-
 class Solution {
 public:
-
     int subarraySum(vector<int>& nums, int k) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        unordered_map<int,int> mp;
-        int cnt=0;
-        int presum=0;
-
-        mp[0]=1;
-        for(int i=0;i<nums.size();i++){
-            presum+=nums[i];
-            int res=presum-k;
+        int cnt=0,sum=0;
+        map<int,int>mp;
+        mp[0]++;
+        for(int num:nums){
+            sum+=num;
+            int res=sum-k;
             cnt+=mp[res];
-            mp[presum]+=1;
-
+            mp[sum]++;
         }
         return cnt;
     }
