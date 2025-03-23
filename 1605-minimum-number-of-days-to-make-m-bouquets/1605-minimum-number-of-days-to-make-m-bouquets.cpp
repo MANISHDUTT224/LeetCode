956e92c8109f411f4712db3db4c6aa9834c35a01
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-23 15:00:12 +0530

    Time: 39 ms (8.85%), Space: 70.5 MB (47.19%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-        int l=1,r=1e9,ans=-1;
-        while(l<=r){
+       
+        int i=0,j=1e9;
+      
+        int ans=-1;
+        while(i<=j){
             int conlen=0,bouqets=0;
-            int mid=l+(r-l)/2;
-            for(int i=0;i<bloomDay.size();i++){
-            if(bloomDay[i]<=mid){
-                conlen++;
-                if(conlen==k){
+            int mid=(i+j)/2;
+            for(int bloomindex=0;bloomindex<bloomDay.size();bloomindex++){
+                if(bloomDay[bloomindex]<=mid){
+                    conlen++;
+                    if(conlen==k){
+                        conlen=0;
+                        bouqets++;
+                    }
+                }
+                else{
                     conlen=0;
-                    bouqets++;
                 }
             }
-            else{
-                conlen=0;
+            if(bouqets>=m){
+                ans=mid;
+                j=mid-1;
             }
-
+            else{
+                i=mid+1;
+            }
         }
-        if(bouqets>=m){
-            ans=mid;
-            r=mid-1;
-        }
-        else{
-            l=mid+1;
-        }
-    }
-    return ans;
+        return ans;
     }
 };
