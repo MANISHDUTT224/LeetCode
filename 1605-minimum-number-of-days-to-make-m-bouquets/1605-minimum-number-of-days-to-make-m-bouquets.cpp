3da17a67781b6be9b828ef37389158ad846c4fc9
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-23 17:33:18 +0530

    Time: 29 ms (24.24%), Space: 70.4 MB (77.23%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    bool possible(vector<int>&arr,int day,int m,int k){
+    bool possible(vector<int>&bloomDay,int mid,int m,int k){
         int conlen=0,bouqets=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]<=day){
-              
+        for(int i=0;i<bloomDay.size();i++){
+            if(bloomDay[i]<=mid){
                 conlen++;
             }
             else{
-            
                 bouqets+=conlen/k;
                 conlen=0;
-            
             }
         }
         bouqets+=conlen/k;
         return bouqets>=m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long val=m*1ll*k*1ll;
-        int n= bloomDay.size();
-        if(val>n){
+        int i=*min_element(bloomDay.begin(),bloomDay.end());
+        int j=*max_element(bloomDay.begin(),bloomDay.end());
+        long long val=k*1ll*m*1ll;
+        if(val>bloomDay.size()){
             return -1;
         }
         int ans=-1;
-        int i=*min_element(bloomDay.begin(),bloomDay.end()),j=*max_element(bloomDay.begin(),bloomDay.end());
         while(i<=j){
             int mid=(i+j)/2;
-            if(possible(bloomDay,mid,m,k)){    
+            if(possible(bloomDay,mid,m,k)){
+                ans=mid;
                 j=mid-1;
             }
             else{
                 i=mid+1;
             }
         }
-        return i;
+        return ans;
     }
 };
