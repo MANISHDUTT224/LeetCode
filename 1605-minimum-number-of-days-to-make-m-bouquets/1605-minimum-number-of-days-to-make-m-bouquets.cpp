3c379fcfb97613db1b7e03f622515d0d3821ab06
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-23 15:16:23 +0530

    Time: 24 ms (48.46%), Space: 70.5 MB (47.19%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-   bool possible(vector<int> &arr, int day, int m, int k) {
-    int n = arr.size(); //size of the array
-    int cnt = 0;
-    int noOfB = 0;
-    // count the number of bouquets:
-    for (int i = 0; i < n; i++) {
-        if (arr[i] <= day) {
-            cnt++;
+    bool possible(vector<int>&arr,int day,int m,int k){
+        int conlen=0,bouqets=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=day){
+              
+                conlen++;
+            }
+            else{
+            
+                bouqets+=conlen/k;
+                conlen=0;
+            
+            }
         }
-        else {
-            noOfB += (cnt / k);
-            cnt = 0;
+        bouqets+=conlen/k;
+        return bouqets>=m;
+    }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long val=m*1ll*k*1ll;
+        int n= bloomDay.size();
+        if(val>n){
+            return -1;
         }
-    }
-    noOfB += (cnt / k);
-    return noOfB >= m;
-}
-int minDays(vector<int> &arr, int m, int k) {
-    long long val = m * 1ll * k * 1ll;
-    int n = arr.size(); //size of the array
-    if (val > n) return -1; //impossible case.
-    //find maximum and minimum:
-    int mini = INT_MAX, maxi = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        mini = min(mini, arr[i]);
-        maxi = max(maxi, arr[i]);
-    }
-
-    //apply binary search:
-    int low = mini, high = maxi;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (possible(arr, mid, m, k)) {
-            high = mid - 1;
+        int ans=-1;
+        int i=*min_element(bloomDay.begin(),bloomDay.end()),j=*max_element(bloomDay.begin(),bloomDay.end());
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(possible(bloomDay,mid,m,k)){    
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
         }
-        else low = mid + 1;
+        return i;
     }
-    return low;
-}
-
 };
