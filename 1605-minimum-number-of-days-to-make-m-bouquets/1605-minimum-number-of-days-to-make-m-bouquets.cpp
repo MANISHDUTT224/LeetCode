82b1a1749e99fcc84a559362048215ae319fe35c
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-03-23 15:02:05 +0530

    Time: 22 ms (63.08%), Space: 70.6 MB (47.19%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-       
-        int i=0,j=1e9;
-      
-        int ans=-1;
-        while(i<=j){
-            int conlen=0,bouqets=0;
-            int mid=(i+j)/2;
-            for(int bloomindex=0;bloomindex<bloomDay.size();bloomindex++){
-                if(bloomDay[bloomindex]<=mid){
-                    conlen++;
-                    if(conlen==k){
-                        conlen=0;
-                        bouqets++;
-                    }
-                }
-                else{
-                    conlen=0;
-                }
-            }
-            if(bouqets>=m){
-                ans=mid;
-                j=mid-1;
-            }
-            else{
-                i=mid+1;
-            }
+   bool possible(vector<int> &arr, int day, int m, int k) {
+    int n = arr.size(); //size of the array
+    int cnt = 0;
+    int noOfB = 0;
+    // count the number of bouquets:
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= day) {
+            cnt++;
         }
-        return ans;
+        else {
+            noOfB += (cnt / k);
+            cnt = 0;
+        }
     }
+    noOfB += (cnt / k);
+    return noOfB >= m;
+}
+int minDays(vector<int> &arr, int m, int k) {
+    long long val = m * 1ll * k * 1ll;
+    int n = arr.size(); //size of the array
+    if (val > n) return -1; //impossible case.
+    //find maximum and minimum:
+    int mini = INT_MAX, maxi = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        mini = min(mini, arr[i]);
+        maxi = max(maxi, arr[i]);
+    }
+
+    //apply binary search:
+    int low = mini, high = maxi;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (possible(arr, mid, m, k)) {
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return low;
+}
+
 };
