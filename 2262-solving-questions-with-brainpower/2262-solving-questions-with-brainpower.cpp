909254a66011fb69ac3be494379418b60b7e0abd
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-04-01 11:57:42 +0530

    Time: 11 ms (32.59%), Space: 127.6 MB (16.19%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long rec(int i,vector<vector<int>>&questions,vector<long long>&dp,int n){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        long long take=questions[i][0]+rec(i+questions[i][1]+1,questions,dp,n);
+        long long dont=rec(i+1,questions,dp,n);
+        return dp[i]=max(take,dont);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n=questions.size();
+        vector<long long>dp(n,-1);
+        return rec(0,questions,dp,n);
+    }
+};
