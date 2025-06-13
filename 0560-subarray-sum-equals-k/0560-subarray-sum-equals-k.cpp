79d90ee6812393f37d717166e7a5ff82542aea89
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-13 23:04:29 +0530

    Time: 69 ms (36.47%), Space: 47.6 MB (35.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,10 +4,12 @@ public:
         int cnt=0,sum=0;
         map<int,int>mp;
         mp[0]++;
-        for(int num:nums){
-            sum+=num;
-            int res=sum-k;
-            cnt+=mp[res];
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            int rem=sum-k;
+            if(mp.find(rem)!=mp.end()){
+                cnt+=mp[rem];
+            }
             mp[sum]++;
         }
         return cnt;
