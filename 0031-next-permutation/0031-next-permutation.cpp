a49e0d57a3607326b7d940077544b90a87ce57e4
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-13 23:09:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
         int ind=-1;
-        for(int i=nums.size()-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 ind=i;
                 break;
@@ -10,15 +11,16 @@ public:
         }
         if(ind==-1){
             reverse(nums.begin(),nums.end());
+            return;
         }
-        else{
-        for(int i=nums.size()-1;i>ind;i--){
+        int i;
+        for( i=n-1;i>ind;i--){
             if(nums[i]>nums[ind]){
                 swap(nums[i],nums[ind]);
                 break;
             }
         }
-        reverse(nums.begin()+ind+1,nums.end());
-        }
+        reverse(nums.begin()+i+1,nums.end());
+    
     }
 };
