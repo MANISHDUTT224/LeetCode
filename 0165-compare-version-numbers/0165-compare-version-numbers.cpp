0de693a346e5d453825399d87f18f345262003ea
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-25 15:43:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i=0,n=version1.size(),j=0,m=version2.size();
+        while(1){
+           long long num1=0,num2=0;
+           while(i<n && version1[i]!='.'){
+            num1=num1*10+version1[i]-'0';
+            i++;
+           }
+           while(j<m && version2[j]!='.'){
+            num2=num2*10+version2[j]-'0';
+           }
+           if(num1>num2){
+            return 1;
+           }
+           else if(num2>num1){
+            return -1;
+           }
+           if(i>=n && j>=m){
+            return 0;
+           }
+           i++;
+           j++;
+        }
+
+        return 0;
+    }
+};
