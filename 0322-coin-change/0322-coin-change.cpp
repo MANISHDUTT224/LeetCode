cd12a4f4c87d2d1e86ba10b2278cc1e20edb506b
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-25 15:19:45 +0530

    Time: 19 ms (83.27%), Space: 22 MB (51.35%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,7 +5,7 @@ public:
         dp[0]=0;
         for(int coin:coins){
             for(int j=coin;j<=amount;j++){
-                dp[j]=max(dp[j-coin]+1,dp[j]);
+                dp[j]=min(dp[j-coin]+1,dp[j]);
             }
         }
         if(dp[dp.size()-1]==amount+1){
