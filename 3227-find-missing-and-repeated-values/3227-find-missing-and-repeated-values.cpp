ebb29f83790f507ac0b17ed6dd0afe5c748dcb2d
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-25 11:27:03 +0530

    Time: 0 ms (100%), Space: 25.7 MB (85.31%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int sum=0;
-        int m=grid[0].size();
-        int xorr=0;
-        map<int,int>mp;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mp[grid[i][j]]==0){
-                sum+=grid[i][j];
+        int n = grid.size();
+        long long sum_n = (long long)n * n * (n * n + 1) / 2;
+        long long sum_squares = (long long)n * n * (n * n + 1) * (2 * n * n + 1) / 6;
+        long long grid_sum = 0, grid_sum_squares = 0;
 
-                }
-                mp[grid[i][j]]++;
-                if(mp[grid[i][j]]>1){
-                    xorr=grid[i][j];
-                }
+        for (const auto& row : grid) {
+            for (int num : row) {
+                grid_sum += num;
+                grid_sum_squares += (long long)num * num;
             }
         }
-        int s;
-         s=((n*m)*(n*m+1))/2;
-        
-        return {xorr,abs(s-sum)};
+
+        long long diff_sum = grid_sum - sum_n;
+        long long diff_sum_squares = grid_sum_squares - sum_squares; 
+        long long sum_ab = diff_sum_squares / diff_sum;
+
+        int a = (diff_sum + sum_ab) / 2;
+        int b = (sum_ab - diff_sum) / 2; 
+
+        return {a, b}; 
     }
 };
