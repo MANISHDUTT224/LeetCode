efb35a7d207bf6a7727539bf7e218dda1abe1121
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-25 15:19:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<long>dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int coin:coins){
+            for(int j=coin;j<=amount;j++){
+                dp[j]=max(dp[j-coin]+1,dp[j]);
+            }
+        }
+        if(dp[dp.size()-1]==amount+1){
+            return -1;
+        }
+        return dp[dp.size()-1];
+    }
+};
