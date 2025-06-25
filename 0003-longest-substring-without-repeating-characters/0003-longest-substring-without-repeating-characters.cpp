155580114772054a9746c52468c567cc17b4b23e
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-06-25 13:09:37 +0530

    Time: 26 ms (14.46%), Space: 16.9 MB (12.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l=0,r=0;
-        int n=s.size();
+        int i=0,r=0;
         set<char>st;
         int curl,maxl=0;
-        while(r<n){
+        while(r<s.size()){
             if(st.find(s[r])==st.end()){
                 st.insert(s[r]);
-                curl=r-l;
+                curl=r-i;
                 maxl=max(maxl,curl+1);
                 r++;
             }
             else{
-                st.erase(s[l]);
-                l++;
+                st.erase(s[i]);
+                i++;
             }
-        
         }
         return maxl;
     }
