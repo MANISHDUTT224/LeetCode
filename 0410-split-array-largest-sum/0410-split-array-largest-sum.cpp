59915ebfb30332aa733a4e1eb6b37075b5b87e8f
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-11 13:35:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    bool possible(vector<int>arr,int n,int k,int mid){
-        int cnt=1;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]>mid){
-               return false;
-            }
-            if(sum+arr[i]>mid){
-                cnt++;
-                sum=arr[i];
+    bool Possible(vector<int>&nums,int mid,int k){
+        int count=1,sm=0;
+        for(int i=0;i<nums.size();i++){
+            if(sm+nums[i]>mid){
+                sm=nums[i];
+                count++;
             }
             else{
-                sum+=arr[i];
+                sm+=nums[i];
             }
         }
-        return cnt<=k;
+        return count<=k;
     }
     int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        int low=min(nums[0],nums[n-1]);
-        int high=sum;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(possible(nums,n,k,mid)){
-                high=mid-1;
+        int i=min(nums[0],nums[n-1]);
+        int j=accumulate(nums.begin(),nums.end(),0);
+        int ans=-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(Possible(nums,mid,k)){
+                ans=mid;
+                j=mid-1;
             }
             else{
-                low=mid+1;
+                i=mid+1;
             }
         }
-        return low;
+        return ans;
     }
 };
