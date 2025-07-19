47d79c4fe29d67b58c5fe045d74e36a869e4c6d2
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-19 09:26:25 +0530

    Time: 42 ms (90.32%), Space: 49.7 MB (68.82%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
         sort(folder.begin(),folder.end());
         vector<string>ans;
         ans.push_back(folder[0]);
         for(int i=1;i<folder.size();i++){
-            string lastfolder=ans.back();
-            lastfolder.push_back('/');
-            if(folder[i].compare(0,lastfolder.size(),lastfolder)!=0){
+            string fold=ans.back();
+            fold.push_back('/');
+            if(folder[i].compare(0,fold.size(),fold)!=0){
                 ans.push_back(folder[i]);
             }
         }
