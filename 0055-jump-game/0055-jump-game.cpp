391199f24e83d0c134052aff764f1d298fe58de5
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-19 10:09:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size(),mx=0;
+        for(int i=0;i<nums.size();i++){
+            if(mx<i){
+                return false;
+            }
+            mx=max(mx,i+nums[i]);
+        }
+        return mx;
+    }
+};
