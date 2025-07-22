55897d5ca321c71c0cc2f2cabd2697e7e91bdf2a
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-23 00:16:39 +0530

    Time: 11 ms (42.11%), Space: 22.9 MB (42.11%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    long getHours(vector<int>&piles,int mid){
-        long total=0;
-        for(int pile:piles){
-            total+=ceil((double)pile/(double)mid);
+    long getHours(vector<int>&arr,long mid){
+        long sum=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=ceil((double)arr[i]/(double)mid);
+
         }
-        return total;
+        return sum;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int i=1,j=*max_element(piles.begin(),piles.end());
-
-        int ans=-1;
+        long i=1;
+        long j=*max_element(piles.begin(),piles.end());
+        long ans=-1;
         while(i<=j){
-            int mid=(i+j)/2;
-            long totalhours=getHours(piles,mid);
-            if(totalhours>h){
-                i=mid+1;
-            }
-            else{
-                ans=mid;
+            long mid=i+(j-i)/2;
+            long hours=getHours(piles,mid);
+            if(hours<=h){
+               
                 j=mid-1;
             }
+            else{
+                i=mid+1;
+            }
         }
-        return ans;
+        return i;
     }
 };
