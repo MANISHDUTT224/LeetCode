2289b598074f134a3055b15f11bd0e2c01f60374
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-23 21:49:34 +0530

    Time: 13 ms (45.43%), Space: 35.1 MB (8.79%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int findDays(vector<int>&weights,int mid){
-        int days=1,load=0;
+    int findays(vector<int>&weights,int mid){
+        int load=0,days=1;
         for(int i=0;i<weights.size();i++){
             if(load+weights[i]>mid){
                 load=weights[i];
@@ -14,13 +14,11 @@ public:
         return days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int i=*max_element(weights.begin(),weights.end());
-        int j=accumulate(weights.begin(),weights.end(),0);
+        int i=*max_element(weights.begin(),weights.end()),j=accumulate(weights.begin(),weights.end(),0);
         int ans=-1;
         while(i<=j){
-            int mid=(i+j)/2;
-            int totaldays=findDays(weights,mid);
-            if(totaldays<=days){
+            int mid=i+(j-i)/2;
+            if(findays(weights,mid)<=days){
                 ans=mid;
                 j=mid-1;
             }
