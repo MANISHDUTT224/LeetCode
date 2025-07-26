3f351827c98f71a4e35296ef2befc7550ee03384
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-07-27 00:27:42 +0530

    Time: 2 ms (33.29%), Space: 10.4 MB (35.91%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         stack<int>st;
-        st.push(-1);
         int maxlen=0;
+        st.push(-1);
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
+            char c=s[i];
+            if(c=='('){
                 st.push(i);
             }
             else{
@@ -13,9 +14,7 @@ public:
                 if(st.empty()){
                     st.push(i);
                 }
-                else{
-                    maxlen=max(maxlen,i-st.top());
-                }
+                maxlen=max(maxlen,i-st.top());
             }
         }
         return maxlen;
