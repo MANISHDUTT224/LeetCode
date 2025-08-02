6905ec598a3622ae14f57e86ade75a1b809513f8
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-08-02 10:42:31 +0530

    Time: 0 ms (100%), Space: 52.4 MB (33.32%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size(),mx=0;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             if(mx<i){
                 return false;
             }
