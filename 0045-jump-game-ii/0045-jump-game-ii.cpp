f2d335c955006382e9690942922acecfb8c60f77
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-09-01 23:03:41 +0530

    Time: 0 ms (100%), Space: 20.4 MB (84.94%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n=nums.size();
         int jumps=0,curfarthest=0,curend=0;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<nums.size()-1;i++){
             curfarthest=max(curfarthest,i+nums[i]);
             if(i==curend){
                 curend=curfarthest;
