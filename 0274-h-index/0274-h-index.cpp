ad0f0d5cd8842c0b967c6797184544c13438e766
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-09-01 23:09:36 +0530

    Time: 0 ms (100%), Space: 12.2 MB (63.52%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,14 +3,15 @@ public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(),citations.end());
         int n=citations.size();
-        int l=0,r=n-1;
+        int l=0,r=citations.size()-1;
         while(l<=r){
             int mid=l+(r-l)/2;
             int h=n-mid;
             if(citations[mid]>=h){
                 r=mid-1;
             }
-            else{
+            else
+            {
                 l=mid+1;
             }
         }
