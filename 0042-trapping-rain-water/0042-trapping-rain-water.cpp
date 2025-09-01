f91dc3904dd983b8161cf8deed26e01f96660c88
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-09-02 01:10:51 +0530

    Time: 0 ms (100%), Space: 27.4 MB (19.98%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -7,16 +7,17 @@ public:
         left[0]=height[0];
         right[n-1]=height[n-1];
         for(int i=1;i<n;i++){
-            left[i]=max(left[i-1],height[i]);
+            left[i]=max(height[i],left[i-1]);
         }
         for(int i=n-2;i>=0;i--){
-            right[i]=max(right[i+1],height[i]);
+            right[i]=max(height[i],right[i+1]);
         }
         int trapped=0;
         for(int i=0;i<n;i++){
-            int waterlevel=min(left[i],right[i]);
+            int waterlevel=min(right[i],left[i]);
             trapped+=waterlevel-height[i];
         }
         return trapped;
+        
     }
 };
