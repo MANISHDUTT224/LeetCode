4bb44525f8b3d2c64978c39e6e70d137d421da23
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-11-15 00:39:41 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.53%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -16,7 +16,8 @@ public:
             return true;
         }
         if(p && q && p->val==q->val){
-            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+           return  isSameTree(p->left,q->left)
+             && isSameTree(p->right,q->right);
         }
         return false;
     }
