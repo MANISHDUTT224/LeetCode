1f875084be3a8b84e76345cfc3b801885b6cbd49
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-11-14 23:27:34 +0530

    Time: 0 ms (100%), Space: 11.3 MB (4.37%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -21,13 +21,13 @@ public:
             prev=prev->next;
         }
         ListNode *curr=prev->next;
-        while(curr){
+        for(int i=0;i<right-left;i++){
             ListNode *temp=curr->next;
             curr->next=temp->next;
             temp->next=prev->next;
             prev->next=temp;
 
         }
-        return dummy->next;
+        return dummyNode->next;
     }
 };
