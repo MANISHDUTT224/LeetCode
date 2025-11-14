b8a53e5b4b7ab701aa99adeeb4b614695b7693d9
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-11-15 00:08:18 +0530

    Time: 0 ms (100%), Space: 23.4 MB (99.6%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -10,20 +10,20 @@
  * };
  */
 class Solution {
-    int res=0;
+     int res=0;
 public:
-    
+   
     int diameterOfBinaryTree(TreeNode* root) {
         dfs(root);
         return res;
     }
-    int dfs(TreeNode* root){
+    int dfs(TreeNode *root){
         if(!root){
             return 0;
         }
         int l=dfs(root->left);
         int r=dfs(root->right);
-        res=max(res,l+r);
-        return 1 +max(l,r);
+        res=max(res,(l+r));
+        return 1+max(l,r);
     }
 };
