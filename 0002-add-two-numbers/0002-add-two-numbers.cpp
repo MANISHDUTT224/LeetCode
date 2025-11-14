f78142e6966b1f3cfb256624d8738915861e3dfd
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2025-11-14 23:09:26 +0530

    Time: 0 ms (100%), Space: 77.1 MB (74.6%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,18 +14,19 @@ public:
         ListNode *dummyHead=new ListNode(0);
         ListNode *curr=dummyHead;
         int carry=0;
-        while(l1 || l2 || carry!=0){
+        while(l1 ||  l2 ||carry){
             int x=l1?l1->val:0;
             int y=l2?l2->val:0;
-            int sum=carry+x+y;
-             carry=sum/10;
-            curr->next=new ListNode(sum%10);
+            int sum=x+y+carry;
+            carry=sum/10;
+            ListNode *node=new ListNode(sum%10);
+            curr->next=node;
             curr=curr->next;
             l1=l1?l1->next:nullptr;
             l2=l2?l2->next:nullptr;
         }
         ListNode *head=dummyHead->next;
-        delete(dummyHead);
+        delete dummyHead;
         return head;
     }
 };
