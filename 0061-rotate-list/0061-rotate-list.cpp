98c0051e50a7873fa8f392eba5045e1dee5e4429
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-05-05 07:51:23 +0530

    Time: 193 ms (0.79%), Space: 16.5 MB (31.2%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,29 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ /*
+ m sbse phle calculate kr rha hun ki 
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        cin.tie(0);
-        cout.tie(0);
-        ios::sync_with_stdio(false);
-       if(head==nullptr){
-        return head;
-       }
-       ListNode *curr=head;
-       int n=1;
-       while(curr->next!=nullptr){
-        n++;
-        curr=curr->next;
-       }
-       k%=n;
-       curr->next=head;
-       curr=head;
-       for(int i=1;i<n-k;i++){
-        curr=curr->next;
-       }
-       head=curr->next;
-       curr->next=nullptr;
+        if(k==0){return head;}  //agr k=0 mtlb no rotation
+       int size = fillq(head); //loop for getting size
+       if(size==0 || size==1){return head;} //agr size 0 to kya rotate kroge
+       while(k>=size){k=k-size;} //to reduce k
+       if(k==0){return head;}
+
+       rot(head,head,k);
+
        return head;
+    }
+
+    void rot(ListNode* &head,ListNode* &l,int k)
+    {
+       static int c=0;
+       if(head!=nullptr)
+       {
+           rot(head->next,l,k);
+           if(c<k){
+           ListNode* t=new ListNode(head->val);
+           t->next=l;
+           l=t;   
+           head=nullptr;c++;}
+       }
+       else{c=0;} //taki static c ka effect na aye
+    }
+
+    int fillq(ListNode* head)
+    {   int count=0;
+        while(head!=nullptr)
+        {
+            head=head->next;
+            count++;
+        }
+        return count;
     }
 };
