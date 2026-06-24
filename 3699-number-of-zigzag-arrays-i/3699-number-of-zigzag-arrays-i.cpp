8c2184b0307f185fcc189dc330c3e94e80f64899
Manish Dutt S <120888214+MANISHDUTT224@users.noreply.github.com>
2026-06-24 08:18:29 +0530

    Time: 696 ms (47.75%), Space: 16.2 MB (62.16%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,17 +1,34 @@
+constexpr int MOD = 1'000'000'007;
+
 class Solution {
 public:
-   static constexpr int MOD = 1000000007;
     int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
-        vector<int> dp(m, 1);
+        vector<int> dp0(m, 0);
+        vector<int> dp1(m, 0);
+        vector<int> sum0(m + 1, 0);
+        vector<int> sum1(m + 1, 0);
 
-        for (int i = 2; i <= n; i++) {
-            reverse(dp.begin(), dp.end());
-            int sum = 0;
-            for (auto& d : dp)
-                sum = (sum + exchange(d, sum)) % MOD;
+        for (int i = 0; i < m; i++) {
+            dp0[i] = dp1[i] = 1;
         }
 
-        return ((accumulate(dp.begin(), dp.end(), 0LL) % MOD) << 1) % MOD;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sum0[j + 1] = (sum0[j] + dp0[j]) % MOD;
+                sum1[j + 1] = (sum1[j] + dp1[j]) % MOD;
+            }
+
+            for (int j = 0; j < m; j++) {
+                dp0[j] = (sum1[m] - sum1[j + 1] + MOD) % MOD;
+                dp1[j] = sum0[j];
+            }
+        }
+
+        auto op = [](int acc, int x) { return (acc + x) % MOD; };
+        auto ans0 = std::reduce(dp0.begin(), dp0.end(), 0, op);
+        auto ans1 = std::reduce(dp1.begin(), dp1.end(), 0, op);
+
+        return (ans0 + ans1) % MOD;
     }
 };
